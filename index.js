a4fdf1c659434a305b061097847178c833594e94
jsliushenGit <1060083868@qq.com>
2019-01-21 20:52:11 +0800

    fix

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-const makeWaterMark = (img, text="jsliushen", {
-  width = 250,
-  height = 200,
-  textAlign = 'center',
-  textBaseline = 'middle',
-  font = '20px 微软雅黑',
-  fillStyle = 'rgba(184, 184, 184, 0.8)',
-  angle = 30,
-  zIndex = 11000,
-} = {}) => {
-  // 创建画布
-  const cvs = document.createElement('canvas');
-  // 画布尺寸
-  cvs.width = width;
-  cvs.height = height;
-  // 上下文
-  const ctx = cvs.getContext('2d');
-  cvs.drawImage(img,0,0);
-  // 文字样式
-  ctx.textAlign = textAlign;
-  ctx.textBaseline = textBaseline;
-  ctx.font = font;
-  ctx.fillStyle = fillStyle;
-  // 文字旋转
-  ctx.rotate(angle * Math.PI / 180);
-  // 绘制文字
-  ctx.fillText(text, width / 2, height / 2);
-  // 生成base64图片编码
-  const base64Url = cvs.toDataURL();
-  return base64Url
-}
-
 module.exports = function (content) {
-  return makeWaterMark(content, 'jsliushen');
+  console.log(content)
+  return content
 };
